3a1404744f97390f6b893c99e994db9b2f97006a
Vivek Katial <vivekkatial@gmail.com>
2023-02-14 15:43:06 +1100

    feat: Adding MAXCUT analysis code for various ANSATZ

--- analysis/matilda-prep-vqe-maxcut.R
+++ analysis/matilda-prep-vqe-maxcut.R
@@ -10,7 +10,7 @@ library(glue)
 
 Sys.time()
 
-RUN_DATETIME <- as.POSIXct("2022-11-23 11:45:00 AEDT", tz = "UTC")
+RUN_DATETIME <- as.POSIXct("2023-01-23 11:45:00 AEDT", tz = "UTC")
 
 # Adding stuff for QAOA
 d_runs <- read_csv("data/d_vqe-maxcut.csv") %>% 
@@ -22,7 +22,9 @@ d_runs <- read_csv("data/d_vqe-maxcut.csv") %>%
 
 instance_classes <- c("uniform_random", "watts_strogatz_small_world", "power_law_tree", "geometric", "nearly_complete_bi_partite", "regular_graph")
 
+
 d_matilda <- map_df(instance_classes, function(instance_class){
+  algos <- c("VQE", "QAOA", "ESU2")
   d_runs %>% 
     select(run_id, contains(instance_class)) %>% 
     mutate(Instances = glue("{run_id}_{instance_class}")) %>% 
@@ -32,7 +34,9 @@ d_matilda <- map_df(instance_classes, function(instance_class){
       list( ~ str_replace(., glue("params.{instance_class}"), "feature"))
     ) %>% 
     rename(
-      energy_gap = glue("metrics.energy_gap_{instance_class}")
+      algo_VQE = glue("metrics.energy_gap_VQE_{instance_class}"),
+      algo_QAOA = glue("metrics.energy_gap_QAOA_{instance_class}"),
+      algo_FVQE = glue("metrics.energy_gap_ESU2_{instance_class}")
     ) %>% 
     mutate_if(is.logical, as.numeric) %>% 
     select_if(~n_distinct(.) > 1) %>% 
@@ -40,7 +44,7 @@ d_matilda <- map_df(instance_classes, function(instance_class){
     select(-contains("feature_number_of_vertices")) %>%
     drop_na() %>% 
     mutate(Source = instance_class) %>% 
-    select(Instances, Source, energy_gap, starts_with("feature"))
+    select(Instances, Source, starts_with("algo"),starts_with("feature"))
 }) %>% 
   filter(feature_laplacian_second_largest_eigenvalue > 0) %>% 
   select(-c("feature_eulerian","feature_bipartite","feature_regular")) %>% 
@@ -75,7 +79,6 @@ testthat::expect_gt(d_matilda %>%
                     1)
 
 d_matilda %>% 
-  rename(algo_energy_gap = energy_gap) %>% 
   select(Instances, Source, starts_with("feature"), starts_with("algo")) %>% 
   write_csv("data/d_matilda.csv")
 
@@ -92,31 +95,5 @@ d_matilda %>%
 d_matilda %>% 
   filter(str_detect(Instances, "3b75")) %>% 
   filter(Source == "nearly_complete_bi_partite") %>% 
-  gather(var, val) %>% 
-  View()
+  gather(var, val)
 
-
-
-
-
-d_dist <- read_csv("~/Documents/HAQC/data/geometric-graphs.csv")
-d_dist %>% 
-  group_by(radius) %>% 
-  count(connected) %>% 
-  ungroup() %>% 
-  group_by(radius) %>% 
-  mutate(perc = n/sum(n)) %>% 
-  filter(connected) %>% 
-  ggplot(aes(x = radius, y = perc)) + 
-  geom_line() + 
-  theme_minimal()
-
-
-d_dist %>% 
-  group_by(radius) %>% 
-  count(connected) %>% 
-  ungroup() %>% 
-  group_by(radius) %>% 
-  mutate(perc = n/sum(n)) %>% 
-  filter(connected) %>% 
-  View()
