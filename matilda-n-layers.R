2ae378ce6c24f91524036330ab9b014b2dd4eb8d
Vivek Katial <vivekkatial@gmail.com>
2023-03-07 13:36:34 +1100

    feat: Add MATILDA n_layers

--- matilda-n-layers.R
+++ matilda-n-layers.R
@@ -11,47 +11,43 @@ library(glue)
 Sys.time()
 
 RUN_DATETIME <- as.POSIXct("2023-01-23 11:45:00 AEDT", tz = "UTC")
+system_size = 13
 
 # Adding stuff for QAOA
 d_runs <- read_csv("data/d_QAOA-Parameter-layers-vanilla.csv") %>% 
   filter(
     start_time > RUN_DATETIME
-  )
+  ) %>% 
+  select(Instances = run_id, Source = params.Source, contains("params"), contains("metrics.energy_QAOA_13")) %>% 
+  select(-contains("size_7"), -metrics.energy_QAOA_13_8,-metrics.energy_QAOA_13_9, -metrics.energy_QAOA_13_10)
 
-d_runs %>% 
-  select(Source=params.Source, contains("params"), contains("metrics.energy_QAOA_13")) %>% 
-  select(-contains("size_7")) %>% 
-  count(Source)
-
-instance_classes <- c("uniform_random", "watts_strogatz_small_world", "power_law_tree", "geometric", "nearly_complete_bi_partite", "regular_graph")
-
-
-d_matilda <- map_df(instance_classes, function(instance_class){
-  algos <- c("VQE", "QAOA", "ESU2")
-  d_runs %>% 
-    select(run_id, contains(instance_class)) %>% 
-    mutate(Instances = glue("{run_id}_{instance_class}")) %>% 
-    select(Instances, everything()) %>% 
-    rename_at(
-      vars(starts_with(glue("params.{instance_class}"))), 
-      list( ~ str_replace(., glue("params.{instance_class}"), "feature"))
+d_matilda <- d_runs %>%
+  rename_at(
+      vars(starts_with(glue("params."))), 
+      list( ~ str_replace(., glue("params."), "feature_"))
     ) %>% 
-    rename(
-      algo_VQE = glue("metrics.energy_gap_VQE_{instance_class}"),
-      algo_QAOA = glue("metrics.energy_gap_QAOA_{instance_class}"),
-      algo_FVQE = glue("metrics.energy_gap_ESU2_{instance_class}")
+    rename_at(
+      vars(starts_with("metrics.energy_QAOA_13")),
+      list(~ str_replace(.,"metrics.energy_QAOA_13" ,"algo_n_layers"))
     ) %>% 
     mutate_if(is.logical, as.numeric) %>% 
     select_if(~n_distinct(.) > 1) %>% 
     select(-contains("feature_acyclic")) %>% 
     select(-contains("feature_number_of_vertices")) %>%
     drop_na() %>% 
-    mutate(Source = instance_class) %>% 
-    select(Instances, Source, starts_with("algo"),starts_with("feature"))
-}) %>% 
-  filter(feature_laplacian_second_largest_eigenvalue > 0) %>% 
-  select(-c("feature_eulerian","feature_bipartite","feature_regular")) %>% 
-  drop_na()
+    select(Instances, Source,starts_with("feature"),starts_with("algo"))
+
+
+# Remove cols with no-variance
+cols_to_remove <- d_matilda %>% 
+  select(starts_with("feature")) %>%
+  summarise(across(everything(), n_distinct)) %>% 
+  gather(var, n_distinct) %>% 
+  filter(n_distinct == 1) %>% 
+  pull(var)
+
+d_matilda <- d_matilda %>% 
+  select(-cols_to_remove)
 
 d_matilda %>% 
   count(Source)
@@ -75,7 +71,24 @@ testthat::expect_equal(d_matilda %>%
                          nrow(),
                        info = "Checking Unique Instances")
 
-# Test that atleast 2 sources
+
+
+
+
+# Tests all feature columns have some variance
+
+testthat::expect_equal(d_matilda %>% 
+                         select(starts_with("feature")) %>%
+                         summarise(across(everything(), n_distinct)) %>% 
+                         gather(var, n_distinct) %>% 
+                         filter(n_distinct == 1) %>% 
+                         nrow(),
+                         0,
+                       info = "There are feature columns with no variance",
+                       label = glue("Here are the issue columns {cols_to_check}")
+)
+
+# Test that atleast
 testthat::expect_gt(d_matilda %>%
                       count(Source) %>%
                       nrow(),
@@ -85,18 +98,4 @@ d_matilda %>%
   select(Instances, Source, starts_with("feature"), starts_with("algo")) %>% 
   write_csv("data/d_matilda.csv")
 
-d_matilda %>% 
-  select(Instances, Source, contains("feature_tsp"), contains("algo")) %>% 
-  count(Source)
-
-
-d_matilda %>% 
-  filter(Source == "geometric") %>% 
-  ggplot(aes(x = feature_density)) +
-  geom_histogram()
-
-d_matilda %>% 
-  filter(str_detect(Instances, "3b75")) %>% 
-  filter(Source == "nearly_complete_bi_partite") %>% 
-  gather(var, val)
 
