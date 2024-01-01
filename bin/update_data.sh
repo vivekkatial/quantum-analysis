3a83439113bd9768a9715ff2b424cd2701e1cee3
Vivek Katial <vivekkatial@gmail.com>
2024-01-01 14:46:07 +1100

    feat: Use Poetry for managing envs

--- bin/update_data.sh
+++ bin/update_data.sh
@@ -8,9 +8,9 @@
 
 #export AQC_EXPERIMENT="three-sat-usa-inc-gs"
 # export QAOA_EXPERIMENT="three-sat-usa-qaoa"
-export QAOA_VRP_EXPERIMENT="QAOA-Parameter-layers-vanilla"
+export QAOA_VRP_EXPERIMENT="QAOA-Instance-Based-Parameter-Optimization"
 
-pipenv run python analysis/extraction-scripts/get_mlflow_data.py --experiment=$QAOA_VRP_EXPERIMENT
+poetry run python analysis/extraction-scripts/get_mlflow_data.py --experiment=$QAOA_VRP_EXPERIMENT
 
 # Run R Script to enrich data with features
 # Rscript analysis/extraction-scripts/build-feature-set.R
